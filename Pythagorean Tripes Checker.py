6e183a5f6ccdec1238ab84314a875d6b0f96c0b7
Sharygin Dmitriy <trajiu@gmail.com>
2019-10-11 19:09:37 +0300

    Add runloop with WHILE TRUE

--- Pythagorean Tripes Checker.py
+++ Pythagorean Tripes Checker.py
@@ -1,12 +1,12 @@
-try:
-    lst_sides = input('Please, enter 3 sides of any triangle (separated by space)'). split()
-    for i in range(len(lst_sides)):
-        lst_sides[i] = int(lst_sides[i])
-    lst_sides.sort()
-    print(lst_sides)
-    if lst_sides[2]**2 == (lst_sides[0]**2 + lst_sides[1]**2):
-        print('Your triangle is a Pythagorean')
-    else:
-        print('Not Pythogorean triangle')
-except Exception:
-    print('Error')
+while True:
+    try:
+        lst_sides = input('Please, enter 3 sides of any triangle (separated by space): '). split()
+        for i in range(len(lst_sides)):
+            lst_sides[i] = int(lst_sides[i])
+        lst_sides.sort()
+        if lst_sides[2]**2 == (lst_sides[0]**2 + lst_sides[1]**2):
+            print('Your triangle is a Pythagorean')
+        else:
+            print('Not Pythogorean triangle')
+    except Exception:
+        print('Error')
