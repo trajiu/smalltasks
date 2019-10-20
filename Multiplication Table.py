f3ac93ee5ba03f0a3a5681fd6ac270a7b6cc190a
Dmitriy <trajiu@gmail.com>
2019-10-20 18:55:38 +0300

    add tasktext

--- Multiplication Table.py
+++ Multiplication Table.py
@@ -1,3 +1,26 @@
+"""
+                            [Project] Multiplication Table
+You remember those multiplication tables from elementary school, right? The ones where 
+you choose a number on the top row and one on the side and see where they meet on the 
+chart? Good. It would be easy enough to just write it out, but let's try using Python 
+to our advantage.
+
+                                        Goal
+Create a program that prints out a multiplication table for the numbers 1 through 9. 
+It should include the numbers 1 through 9 on the top and left axises, and it should be 
+relatively easy to find the product of two numbers. Do not simply write out every line 
+manually (ie print('7 14 21 28 35 49 56 63') ).
+
+                                        Subgoals
+As your products get larger, your columns will start to get crooked from the number of 
+characters on each line. Clean up your table by evenly spacing columns so it is very easy 
+to find the product of two numbers.
+
+Allow the user to choose a number to change the size of the table (so if they type in 12, 
+the table printed out should be a 12x12 multiplication table).
+
+"""
+
 while True:
     try:
         input_count = input('Введите размер: ')
